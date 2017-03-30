b5558e00e817cea20499daafdbae443690e13e12
Dmitry Zapashchikov <dmitriiz@hotmail.com>
2017-03-30 08:28:20 +0300

    doing exercises

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
@@ -1,11 +1,12 @@
 package com.fpinjava.functions.exercise02_03;
 
+import com.fpinjava.functions.listing02_02.Function;
 
 public class FunctionExamples {
 
-  public static final Function<Integer, Function<Integer, Integer>> add = null; // To be implemented
+  public static final Function<Integer, Function<Integer, Integer>> add = arg1 -> arg2 -> arg1 + arg2;
 
-  public static final BinaryOperator add2 =  null; // To be implemented
+  public static final BinaryOperator add2 = arg1 -> arg2 -> arg1 + arg2;
 
-  public static final BinaryOperator mult =  null; // To be implemented
+  public static final BinaryOperator mult =  arg1 -> arg2 -> arg1 * arg2;
 }
--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
@@ -7,7 +7,8 @@ public class FunctionExamples {
   public static final Function<Integer, Integer> square = x -> x * x;
 
   public static final Function<Function<Integer, Integer>, Function<Function<Integer, Integer>,
-                                        Function<Integer, Integer>>> compose = null; // To be implemented
+                                        Function<Integer, Integer>>> compose =
+          arg1 -> arg2 -> arg -> arg1.apply(arg2.apply(arg));
 
   public static final Function<Integer, Integer> f = compose.apply(square).apply(triple);
 }
